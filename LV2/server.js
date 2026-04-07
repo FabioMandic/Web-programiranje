c586f46c92fff9442c826c4f0b9b23a55e02bf19
FabioMandic <168546186+FabioMandic@users.noreply.github.com>
2026-04-07 14:43:31 +0200

    Update server.js

--- LV2/server.js
+++ LV2/server.js
@@ -8,18 +8,19 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // ZADATAK 3: Ruta za dinamičku galeriju
 app.get('/slike', (req, res) => {
-    const dataPath = path.join(__dirname, 'images.json');
-    const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-    res.render('slike', { slike: data });
+    const dataPath = path.join(__dirname, 'image.json');
+    const rawData = fs.readFileSync(dataPath);
+    const imagesData = JSON.parse(rawData);
+    
+    res.render('slike', { slike: imagesData });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server radi! Galerija je na: http://localhost:${PORT}/slike`);
+    console.log(`Server pokrenut! Provjeri: http://localhost:${PORT}/slike`);
 });

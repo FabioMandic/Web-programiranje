27dd8a5284b1cc48b919fc2cc3ec4d567c7b01be
FabioMandic <168546186+FabioMandic@users.noreply.github.com>
2026-04-07 14:26:55 +0200

    Update server.js

--- LV2/server.js
+++ LV2/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Railway dinamički dodjeljuje port 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
-// Posluživanje statičkih datoteka iz mape 'public' [cite: 237, 543]
-app.use(express.static('public')); 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server pokrenut na portu ${PORT}`);
+    console.log(`Server radi na portu ${PORT}`);
 });
